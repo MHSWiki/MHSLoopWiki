6f2f37507ed8bee5b61700b2b0bd1ce4c4f11a4e
Ryan <newmanityglobal@gmail.com>
2026-03-04 22:59:57 +0700

    Corrected site URL to mhsloopwiki.netlify.app

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mhsloop.netlify.app',
+	site: 'https://mhsloopwiki.netlify.app',
 	integrations: [
 		starlight({
 			title: 'MHS Loop Wiki',
